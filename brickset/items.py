f9bb13401bd7b3f7bc9c1e4f110306e21cda7df8
Professor Wergeles <wergelesn@missouri.edu>
2019-10-28 21:19:13 -0500

    After class version 1: This is the exact same code as we did in class just fixing the imports ... however this code is outdated ... if you go to AfterClass V2 that is the most updated way to connect Scrapy with Mongo

--- brickset/items.py
+++ brickset/items.py
@@ -3,7 +3,7 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
+# https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
@@ -11,4 +11,7 @@ import scrapy
 class BricksetItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    #pass
+    name = scrapy.Field()
+    pieces = scrapy.Field()
+    image = scrapy.Field()
--- brickset/pipelines.py
+++ brickset/pipelines.py
@@ -3,9 +3,35 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+
+#from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+#from scrapy import log
+import logging
 
 class BricksetPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient("localhost",27017)
+        db = connection["brickset"]
+        self.collection = db["sets"]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+#            log.msg("Question added to MongoDB database!", level=log.DEBUG, spider=spider)
+            logging.log(logging.DEBUG, "Question added to MongoDB database!")
         return item
